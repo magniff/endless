c6f99b81fb126a98f3de42f80491cb4d9be40a6e
magniff <artcollect@inbox.ru>
2015-10-31 17:29:17 +0300

    Add some info into readme file.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # restack
+
+Infinite recursion in python, have a look at tests :)
--- tests/test_basics.py
+++ tests/test_basics.py
@@ -9,3 +9,16 @@ def test_factorial():
 
     assert fac(n=10) == 3628800
     assert fac(n=10000) == fac(n=9999) * 10000
+
+
+def test_palindrome():
+
+    @restack.decorate
+    def is_palindrome(word):
+        return (
+            len(word) in [0, 1] or word[0] == word[-1] and
+            (yield {'word': word[1:-1]})
+        )
+
+    assert not is_palindrome(word='some arbitary text')
+    assert is_palindrome(word='abcddcba')
